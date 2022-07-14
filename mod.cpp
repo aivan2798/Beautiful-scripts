3cade845106e9f5cea1d8f1ece7fb152e740c9b5
aivan2798 <cot2we@gmail.com>
2022-07-14 06:53:31 +0300

    Add files via upload

---
+++ mod.cpp
@@ -0,0 +1,44 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+
+
+int mod(int x,int y)
+{
+ 
+ return x-(y*(x/y));
+
+}
+
+int modx(int li,int d1,int d2,int w)
+{
+  
+  return ((li+(d1*w))*(d2-1))/d2;
+}
+
+int modxx(int li,int d1,int d2,int w)
+{
+  
+  return ((li*(d2-1))-(d1*w)*(d2-1))/d2;
+  
+}
+
+int gcm_lim(int li,int d1,int d2)
+{
+  int p1=pow(d2,2)*li;
+//  cout<<"\np1: "<<(p1);
+  int p2=(d2*li);
+//  cout<<"\np2: "<<p2;
+  int dt1=(d1*pow(d2,2));
+//  cout<<"\ndt1: "<<dt1;
+  int dt2=(d1*d2);
+ // cout<<"\ndt2: "<<dt2;
+  return (p1-p2)/(dt1+dt2);
+}
+
+
+int main()
+{
+ cout<<endl<<gcm_lim(30,6,8);
+ cout<<"\nfinished";
+}
